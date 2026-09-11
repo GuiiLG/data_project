aef1182bf34ad93736354956ee7cf06c34dbbcb3
Luigi <guiluigiribeiro@gmail.com>
2026-09-11 10:46:54 -0300

    convert csv to json

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,9 +1,11 @@
 import  json
+import csv
 from pathlib import Path
 
 current_directory = Path(__file__).resolve().parent
 output_dir = current_directory.parent / "data" / "processed"
 data_dir = current_directory.parent / "data" / "raw"
+output_dir.mkdir(parents=True, exist_ok=True)
 
 files = [
     "olist_customers_dataset.csv",
@@ -19,5 +21,6 @@ all_files = {}
 for file_name in files:
     with open(f"{data_dir}/{file_name}", "r", encoding="utf-8") as f:
         file_name_no_csv = file_name.replace(".csv", "")
-        all_files[file_name_no_csv] = f.readlines()
-
+        all_files[file_name_no_csv] = list(csv.DictReader(f))
+    with open(f"{output_dir}/{file_name_no_csv}.json", "w", encoding="utf-8") as f:
+        json.dump(all_files[file_name_no_csv], f, indent=2)
