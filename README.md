04be232740beee89a5e857cff55a16145de2c065
Luigi <guiluigiribeiro@gmail.com>
2026-09-11 09:27:56 -0300

    download datasets configured

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # data_project
+
+REPO SETUP:
+
+git clone https://github.com/GuiiLG/data_project.git
+uv sync
+uv run python src/download_data.py
+uv run python src/pipeline.py
--- src/download_data.py
+++ src/download_data.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+from pathlib import Path
+
+# Set the path to the file you'd like to load
+current_directory = Path(__file__).resolve().parent
+file_path = current_directory.parent / "data" / "raw"
+
+
+# Load the latest version
+for file_name in [
+    "olist_customers_dataset.csv",
+    "olist_order_items_dataset.csv",
+    "olist_order_reviews_dataset.csv",
+    "olist_orders_dataset.csv",
+    "olist_products_dataset.csv",
+    "product_category_name_translation.csv"
+]:
+    kagglehub.dataset_download(
+        "olistbr/brazilian-ecommerce",
+        path=file_name,
+        output_dir=str(file_path),
+    )
+
