84b934cd215fea9bfd3f775dd155f028965c982e
Luigi <guiluigiribeiro@gmail.com>
2026-09-16 09:46:38 -0300

    Height and Weight cleaned and converted

--- src/clean_structure.py
+++ src/clean_structure.py
@@ -2,6 +2,13 @@ def clean_structure(datasets):
     for player in datasets["fifa21_raw_data"]:
         for field, value in player.items():
             player[field] = value.strip().replace("\n", " ")
+            if field == "Height":
+                player[field] = player[field].replace('"',"")
+                height = player[field].split("'")
+                player[field] = round((float(height[0]) * 0.3048) + (float(height[-1]) * 0.0254),2)
+            if field == "Weight":
+                player[field] = player[field].replace("lbs", "")
+                player[field] = round(float(player[field]) * 0.453592,2)
             if value.isdigit():
-                player[field] = int(value)
-        print(player)   
+                player[field] = int(value) 
+        print(player["Joined"])
