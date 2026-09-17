7c660d7171555bde2cc6acc28dede3d0bc0af8d6
Luigi <guiluigiribeiro@gmail.com>
2026-09-17 17:38:23 -0300

    TO DO updated

--- src/check_for_nulls.py
+++ src/check_for_nulls.py
@@ -1,6 +1,11 @@
 def check_for_nulls(dataset):
+    mandatory_columns = ["LongName","playerUrl","Nationality","Positions","Name","Age","↓OVA","ID","Height","Weight","foot","Growth"]
     for player in dataset:
         for field, value in player.items():
             if value == None or value == "N/A" :
                 player[field] = None
+            if field in mandatory_columns:
+                if value == None:
+                    print(f"Player {player["Name"]} miss some important info, for that reason will be removed from the dataset!")
+                    dataset.remove(player)
     return dataset
